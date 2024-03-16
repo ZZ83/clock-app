c714fe284dfd218981dfd9b22e7535318bcde74b
ZZ83 <13670661+ZZ83@users.noreply.github.com>
2024-03-15 19:19:16 -0700

    Add logic to show and hide footer

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+const primaryButton = document.querySelector("#primary");
 
+primaryButton.addEventListener("click", () => {
+    const body = document.querySelector("body");
+    const arrow = document.querySelector(".arrow");
+    if (body.classList.contains("lift")) {
+        body.classList.remove("lift");
+        primaryButton.childNodes[0].nodeValue = "MORE";
+        arrow.style.transform = "rotate(0deg)";
+    } else {
+        body.classList.add("lift");
+        primaryButton.childNodes[0].nodeValue = "LESS";
+        arrow.style.transform = "rotate(180deg)";
+    }
+});
