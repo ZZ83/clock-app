6bbf886741b13408bb26eac1354abf2dad53d727
ZZ83 <13670661+ZZ83@users.noreply.github.com>
2024-03-17 18:21:55 -0700

    Add initial date logic
    Add logic to display the day of the week, the day of the year, and
    the week number

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
+/***************** MODULE *****************/
 const primaryButton = document.querySelector("#primary");
-
 primaryButton.addEventListener("click", () => {
     const body = document.querySelector("body");
     const arrow = document.querySelector(".arrow");
@@ -13,3 +13,23 @@ primaryButton.addEventListener("click", () => {
         arrow.style.transform = "rotate(180deg)";
     }
 });
+
+/***************** MODULE *****************/
+const todaysDate = new Date();
+const dayOfTheYear = document.querySelector("#day-of-the-year");
+const dayOfTheWeek = document.querySelector("#day-of-the-week");
+const weekNumber = document.querySelector("#week-number");
+const getDateNumbers = (todaysDate) => {
+    const firstDayOfTheYear = new Date(todaysDate.getFullYear(), 0, 1);
+    const timeDifference = todaysDate - firstDayOfTheYear;
+    const millisecondsInADay = 86400000;
+    const dayOfTheYear = Math.floor(timeDifference / millisecondsInADay + 1);
+    const weekNumber = Math.ceil(dayOfTheYear / 7);
+
+    console.log(todaysDate.toLocaleString("en-US"));
+
+    return [dayOfTheYear, weekNumber];
+};
+dayOfTheWeek.innerText = todaysDate.getDay();
+dayOfTheYear.innerText = getDateNumbers(todaysDate)[0];
+weekNumber.innerText = getDateNumbers(todaysDate)[1];
