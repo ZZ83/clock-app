7f5c9288755ec934cc72f8288a0ad74bb6d2357c
ZZ83 <13670661+ZZ83@users.noreply.github.com>
2024-03-20 20:42:54 -0700

    Finish adding logic to display API data

--- script.js
+++ script.js
@@ -35,19 +35,47 @@ primaryButton.addEventListener("click", () => {
     }
 });
 
-/***************** MODULE *****************/
-const todaysDate = new Date();
-const dayOfTheYear = document.querySelector("#day-of-the-year");
-const dayOfTheWeek = document.querySelector("#day-of-the-week");
-const weekNumber = document.querySelector("#week-number");
-const getDateNumbers = (todaysDate) => {
-    const firstDayOfTheYear = new Date(todaysDate.getFullYear(), 0, 1);
-    const timeDifference = todaysDate - firstDayOfTheYear;
-    const millisecondsInADay = 86400000;
-    const dayOfTheYear = Math.floor(timeDifference / millisecondsInADay + 1);
-    const weekNumber = Math.ceil(dayOfTheYear / 7);
-    return [dayOfTheYear, weekNumber];
-};
-dayOfTheWeek.innerText = todaysDate.getDay();
-dayOfTheYear.innerText = getDateNumbers(todaysDate)[0];
-weekNumber.innerText = getDateNumbers(todaysDate)[1];
+function getData() {
+    const currentTime = document.querySelector("#current-time");
+    const abbreviation = document.querySelector("#abbreviation");
+    const cityCountry = document.querySelector("#city-country");
+    const timezone = document.querySelector("#timezone");
+    const dayOfTheYear = document.querySelector("#day-of-the-year");
+    const dayOfTheWeek = document.querySelector("#day-of-the-week");
+    const weekNumber = document.querySelector("#week-number");
+
+    fetch("http://worldtimeapi.org/api/ip")
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error("Network response was not ok please try again.");
+            }
+            return response.json();
+        })
+        .then((data) => {
+            const dateTime = new Date(data.datetime);
+            currentTime.innerText = `${dateTime.getHours()}:${
+                dateTime.getMinutes() < 10 ? "0" : "" + dateTime.getMinutes()
+            }`;
+            abbreviation.innerText = data.abbreviation;
+            timezone.innerText = data.timezone;
+            dayOfTheWeek.innerText = data.day_of_week;
+            dayOfTheYear.innerText = data.day_of_year;
+            weekNumber.innerText = data.week_number;
+
+            return fetch(`http://ip-api.com/json/${data.client_ip}`);
+        })
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error("Network response was not ok please try again.");
+            }
+            return response.json();
+        })
+        .then((data2) => {
+            cityCountry.innerText = `In ${data2.city}, ${data2.countryCode}`;
+        })
+        .catch((error) => {
+            alert(error.message);
+        });
+}
+
+getData();
